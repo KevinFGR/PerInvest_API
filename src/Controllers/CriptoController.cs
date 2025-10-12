00d01af5c830bf67bfac4301bdca8a97c8ad0d2b
Kevin_FGR <kevinfelipe.gr@gmail.com>
2025-10-11 22:01:41 -0300

    feat: complete cripto crud

--- src/Controllers/CriptoController.cs
+++ src/Controllers/CriptoController.cs
@@ -5,6 +5,7 @@ using PerInvest_API.src.Dtos;
 using PerInvest_Api.src.Models.Criptos;
 using System.Linq.Expressions;
 using PerInvest_API.src.Dtos.Shared;
+using PerINvest_API.src.Helpers;
 
 namespace PerInvest_API.src.Controllers;
 
@@ -12,10 +13,10 @@ public class CriptoController :IEndpoint
 {
     public static void Map(IEndpointRouteBuilder app)
     {
-        app.MapGet("/", Get).Produces<dynamic>();
-        app.MapPost("/", Add).Produces<dynamic>();
+        app.MapGet("/", Get);
+        app.MapPost("/", Add);
         app.MapPut("/", Update);
-        app.MapDelete("/", Delete);
+        app.MapDelete("/{id}", Delete);
     }
 
     public static async Task<IResult> Get(AppDbContext context)
@@ -31,19 +32,20 @@ public class CriptoController :IEndpoint
         }
     }
 
-    public static async Task<dynamic> Add(AppDbContext context, CreateCriptoDto request)
+    public static async Task<IResult> Add(AppDbContext context, CreateCriptoDto request)
     {
         try
         {
-            Cripto cripto = new(){
-                Description = request.Description,
-                CreatedAt = DateTime.Now,
-                UpdatedAt = DateTime.Now
-            };
+
+            Cripto cripto = request.MapV2<Cripto>();
+            cripto.CreatedAt = DateTime.Now;
+            cripto.UpdatedAt = DateTime.Now;
+            cripto.CreatedBy = request.UserId;
+            cripto.UpdatedBy = request.UserId;
 
             await context.Criptos.InsertOneAsync(cripto);
 
-            return new Response(200).Result;
+            return new Response(201, "Cripto criada com sucesso").Result;
         }
         catch(Exception ex)
         {
@@ -51,19 +53,20 @@ public class CriptoController :IEndpoint
         }
     }
 
-    public static async Task<dynamic> Update(AppDbContext context, UpdateCriptoDto request)
+    public static async Task<IResult> Update(AppDbContext context, UpdateCriptoDto request)
     {
         try
         {
             Cripto cripto = await context.Criptos.Find(x => x.Id == request.Id && !x.Deleted).FirstOrDefaultAsync();
             cripto.Description = request.Description;
+            cripto.Color = request.Color;
             cripto.UpdatedBy = request.UserId;
             cripto.UpdatedAt = DateTime.Now;
 
             Expression<Func<Cripto, bool>> filter = x => x.Id == request.Id && !x.Deleted;
             await context.Criptos.ReplaceOneAsync(filter, cripto);
 
-            return new Response(200).Result;
+            return new Response(200, "Cripto Atualizada com sucesso").Result;
         }
         catch(Exception ex)
         {
@@ -71,18 +74,19 @@ public class CriptoController :IEndpoint
         }
     }
 
-    public static async Task<dynamic> Delete(AppDbContext context, DeleteRequest request)
+    public static async Task<IResult> Delete(AppDbContext context, HttpContext httpContext, string id)
     {
         try
         {
-            Expression<Func<Cripto, bool>> filter = x => x.Id == request.Id;
+            DeleteRequest request = new (httpContext, id);
+            Expression<Func<Cripto, bool>> filter = x => x.Id == id;
             var update = Builders<Cripto>.Update
                 .Set(x => x.Deleted, true)
                 .Set(x => x.DeletedAt, DateTime.Now)
                 .Set(x => x.DeletedBy, request.UserId);
             await context.Criptos.UpdateOneAsync(filter, update);
 
-            return new Response(200).Result;
+            return new Response(200, "Cripto Excluida com sucesso").Result;
         }
         catch(Exception ex)
         {
